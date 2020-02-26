28299f321dee5240f19917f53ff6cd0fc7426014
Rob-Swensen <rg.swensen801@gmail.com>
2020-02-25 18:35:24 -0700

    Final Commit

---
+++ src/Components/CoverImage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const CoverImage = () => {
+    return(
+        <div>
+            <h1>Rob</h1>
+            <h2>Spirit Animal:</h2>
+            <img width={'400px'}src="https://www.naturalworldsafaris.com/media/5533/nws-st-siberian-tiger-close-medium.jpg" alt=""/>
+        </div>
+
+    )
+}
+
+export default CoverImage
